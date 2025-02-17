989784e332b375802638f5ea646d49bf555008bc
hnymlkd <dinarabagzhankyzy@gmail.com>
2025-02-17 19:39:37 +0500

    added TaskController

---
+++ src/main/java/kz/bitlab/techorda2025/thirdProject/controllers/TaskController.java
@@ -0,0 +1,13 @@
+package kz.bitlab.techorda2025.thirdProject.controllers;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TaskController {
+    @GetMapping(value="/tasks")
+    public String getTasks() {
+        return "tasks";
+    }
+}
